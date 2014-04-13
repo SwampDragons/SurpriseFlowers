4a5eea294db35e61a54ee44559643b1c61f189a7
Megan Marsh <megan@saucelabs.com>
2014-04-13 18:35:58 -0400

    way too many changes
    made test classes reflect new object oriented SurpriseFlowers class
    
    created database and initialized flask app

--- flowers.py
+++ flowers.py
@@ -1,10 +1,10 @@
-# This program exists to remind Chris to buy me flowers "just because" on a monthly basis.  
+# This program exists to remind Chris to buy me flowers "just because" on a monthly basis.
 # It is called by the following cron job once per day:
 # 0 0 1  * * /usr/local/bin/python /Users/mmarsh/Projects/SurpriseFlowers/flowers.py
 #
 # next steps:
 #   1. Have option to select frequency of flowers (e.g. every two weeks or bimonthly)
-#   2. Send additional reminders for birthdays, anniversaries, 
+#   2. Send additional reminders for birthdays, anniversaries,
 #      and holidays where flowers are expected
 #   3. post event to google calendar with reminder for flowers
 #
@@ -19,126 +19,137 @@ username = 'none'
 password = 'none'
 
 try:
-    import local_settings as settings
-    username = settings.USERNAME
-    password = settings.PASSWORD
-    fromaddr = settings.FROMADDR
-    toaddr = settings.TOADDR 
+    import email_config
+    username = email_config.USERNAME
+    password = email_config.PASSWORD
+    fromaddr = email_config.FROMADDR
+    toaddr = email_config.TOADDR
+    flower_sender_name = email_config.SENDERNAME
+    flower_recipient_name = email_config.RECIPIENTNAME
 except ImportError:
-    pass
+    raise Warning("could not find local email_config")
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 filename = os.path.join(PROJECT_ROOT, 'flowerdate.txt')
 
-def date_generator(todays_date):
-    # This function picks a random day of the current month and saves it to flowermap
+class SurpriseFlowers(object):
 
-    # generate a random integer from 1 : the number of days in the month
-    _, ndays = calendar.monthrange(todays_date.year, todays_date.month)
-    flowerday = random.choice(range(1,ndays+1))
-    # print 'flowerday is...',flowerday
-    recentmonth = todays_date.month
-    # print 'recent month is...', recentmonth
-    flowermap = {'flowerday':flowerday, 'current_month':recentmonth, 'email_sent':False}
-    with open(filename, 'w') as f:
-        f.write(json.dumps(flowermap))
-        # add todays_date to flowerdates.txt
-    return flowermap
+    def __init__(self, date=None, flowermap=None):
+        self.date = date
+        self.flowermap = flowermap
+        if not self.date:
+            self.date = datetime.date.today()
+        if not self.flowermap:
+            self.flowermap = {}
 
-def email_chris(flowermap, todays_date):
-    # This function creates and sends the email, using smtplib
-    #email_subject = "Subject: Surprise Flowers!\n\n"
-    # msg['Subject'] = 'The contents of %s' % textfile
-    # msg['From'] = me
-    # msg['To'] = you
 
-    msg = """Subject: Surprise Flowers!\n\n
-    Megan is great.\n
-    I love Megan.\n
-    You know how I can tell her she's great?  FLOWERS!\n
-    I should buy her flowers."""
-
-    try:
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.starttls()
-        server.login(username, password)
-        server.sendmail(fromaddr, toaddr, msg)
-    except smtplib.SMTPAuthenticationError as e:
-        print e
-    finally:
-        server.quit()
-
-    # with open(os.path.join(PROJECT_ROOT,'temp.txt'), 'a') as f:
-        # f.write('\n email Chris {0}'.format(datetime.datetime.now()))
-    with open(filename, 'w') as f:
-        flowermap['flowerday'] = todays_date.day
-        flowermap['current_month'] = todays_date.month
-        flowermap['email_sent'] = True
-        f.write(json.dumps(flowermap))
-
-def check_file(filename):
-    # This function reads flowermap from the save fie; 
-    # if no save file exists then this function creates the file for the first time  
-
-    file_status = '-1'
-
-    if not os.path.isfile(filename):
-        # print 'creating flowerdate.txt...'
-        flowermap = {'flowerday':None, 'current_month':None, 'email_sent':False}
-        # print flowermap
+    def date_generator(self):
+        # generate a random integer from 1 : the number of days in the month
+        _, ndays = calendar.monthrange(self.date.year, self.date.month)
+        flowerday = random.choice(range(1,ndays+1))
+        # print 'flowerday is...',flowerday
+        recentmonth = self.date.month
+        # print 'recent month is...', recentmonth
+        self.flowermap = {'flowerday':flowerday, 'current_month':recentmonth, 'email_sent':False}
         with open(filename, 'w') as f:
-            f.write(json.dumps(flowermap))
-        file_status = 'created file'
+            f.write(json.dumps(self.flowermap))
+            # add self.date to flowerdates.txt
+        return self.flowermap
 
-    # file already exists, so read it
-    else:  
-        with open(filename, 'r') as f:
-            flowermap = json.loads(f.read())     
-        file_status = 'read file' 
+    def send_email(self):
+        # This function creates and sends the email, using smtplib
+        # email_subject = "Subject: Surprise Flowers!\n\n"
+        # msg['Subject'] = 'The contents of %s' % textfile
+        # msg['From'] = me
+        # msg['To'] = you
 
-    # return the contents of the file
-    return flowermap, file_status
-    
-def check_date_for_emailer(flowermap, todays_date, callback):
-    # This function determines based on flowermap whether it is time to send the email
+        msg = """Subject: Surprise Flowers!\n\n
+        {0} is great.\n
+        I love {0}.\n
+        You know how I can tell {0} that {0} is great?  FLOWERS!\n
+        I should buy {0} flowers."""
+        msg.format(RECIPIENTNAME)
 
-    email_status = 'email not sent'
+        try:
+            server = smtplib.SMTP('smtp.gmail.com:587')
+            server.starttls()
+            server.login(username, password)
+            server.sendmail(fromaddr, toaddr, msg)
+        except smtplib.SMTPAuthenticationError as e:
+            print e
+        finally:
+            server.quit()
 
-    # if today's date matches this month's generated date, run emailer
-    if flowermap['flowerday'] <= todays_date.day and \
-    flowermap['current_month'] == todays_date.month and flowermap['email_sent'] == False:
-        email_status = 'email sent'
-        callback(flowermap, todays_date)
+        # with open(os.path.join(PROJECT_ROOT,'temp.txt'), 'a') as f:
+            # f.write('\n email Chris {0}'.format(datetime.datetime.now()))
+        with open(filename, 'w') as f:
+            self.flowermap['flowerday'] = self.date.day
+            self.flowermap['current_month'] = self.date.month
+            self.flowermap['email_sent'] = True
+            f.write(json.dumps(self.flowermap))
 
-    return email_status
+    def check_file(filename):
+        # This function reads self.flowermap from the save fie;
+        # if no save file exists then this function creates the file for the first time
 
-def check_date_for_generator(flowermap, todays_date): 
-    #This function determines whether it is time to generate a new random day of the month
+        file_status = '-1'
 
-    generator_status = 'date not generated'
+        if not os.path.isfile(filename):
+            # print 'creating flowerdate.txt...'
+            self.flowermap = {'flowerday':None, 'current_month':None, 'email_sent':False}
+            # print self.flowermap
+            with open(filename, 'w') as f:
+                f.write(json.dumps(self.flowermap))
+            file_status = 'created file'
 
-    # if today is the first day of the month
-    # or the first time we ran the program this month pick a random day of the month to buy flowers for me.
-    if (todays_date.day >= 1 and flowermap['current_month'] < todays_date.month) or \
-            flowermap['flowerday'] == None:
+        # file already exists, so read it
+        else:
+            with open(filename, 'r') as f:
+                self.flowermap = json.loads(f.read())
+                # print self.flowermap
+            file_status = 'read file'
 
-        generator_status = 'date was generated'
-        flowermap = date_generator(todays_date)
+        # return the contents of the file
+        return self.flowermap, file_status
 
-    return flowermap, generator_status
+    def check_date_for_emailer(self, callback):
+        # This function determines based on self.flowermap whether it is time to send the email
 
-def main(todays_date, callback):
-    # if no save file existed before today, initialize the flowermap variable and save it to a file
-    flowermap, file_status = check_file(filename)
+        email_status = 'email not sent'
+        # if today's date matches this month's generated date, run emailer
+        if self.flowermap['flowerday'] <= self.date.day and \
+        self.flowermap['current_month'] == self.date.month and self.flowermap['email_sent'] == False:
+            email_status = 'email sent'
+            callback(self.flowermap, self.date)
+
+        return email_status
+
+    def check_date_for_generator(self):
+        #This function determines whether it is time to generate a new random day of the month
+
+        generator_status = 'date not generated'
+
+        if ((self.date.day >= 1 and
+            (self.flowermap['current_month'] < self.date.month) or
+            self.flowermap['current_month'] == 12 and self.date.month == 1) or
+                self.flowermap['flowerday'] == None):
+            generator_status = 'date was generated'
+            self.flowermap = self.date_generator()
+        # print generator_status
+        return self.flowermap, generator_status
+
+def main(self, callback):
+    # if no save file existed before today, initialize the self.flowermap variable and save it to a file
+    self.flowermap, file_status = check_file(filename)
 
     # in no file existed before today, generate a legitimate date
-    flowermap, generator_status = check_date_for_generator(flowermap, todays_date)
+    self.flowermap, generator_status = check_date_for_generator(self.flowermap, self.date)
 
     # check whether we should email the person
-    email_status = check_date_for_emailer(flowermap, todays_date, callback)
+    email_status = check_date_for_emailer(self.flowermap, self.date, callback)
 
 if __name__ == '__main__':
-    main(datetime.date.today(), email_chris)
+    main(datetime.date.today(), send_email)
 
 
 
--- flowertest.py
+++ flowertest.py
@@ -3,97 +3,60 @@ import datetime
 import json
 import os
 import unittest
-
 import flowers
 
-class FlowerTest(unittest.TestCase):
+## test the method to create a file if it isn't present
+#class FlowerTest(unittest.TestCase):
+#
+#    def test_check_file_present(self):
+#        expected = {"flowerday": 20, "current_month": 6, "email_sent": True}
+#        with open(flowers.filename, 'w') as f:
+#            f.write(json.dumps(expected))
+#        flowermap, file_status = flowers.check_file(flowers.filename)
+#        self.assertEquals(flowermap,expected)
+#        os.remove(flowers.filename)
+#
+#    def test_check_file_absent(self):
+#        if os.path.isfile(flowers.filename):
+#            os.remove(flowers.filename)
+#
+#        expected = {'flowerday':None, 'current_month':None, 'email_sent':False}
+#        flowermap, file_status = flowers.check_file(flowers.filename)
+#        self.assertEquals(flowermap, expected)
+#
+#        with open(flowers.filename, 'r') as f:
+#            flowermap = json.loads(f.read())
+#        self.assertEquals(flowermap, expected)
 
-    # test the method to create a file if it isn't present
-    def test_check_file_present(self):
-        expected = {"flowerday": 20, "current_month": 6, "email_sent": True}
-        with open(flowers.filename, 'w') as f:
-            f.write(json.dumps(expected))
-        flowermap, file_status = flowers.check_file(flowers.filename)
-        self.assertEquals(flowermap,expected)
-        os.remove(flowers.filename)
+class TestSurpriseFlowers(unittest.TestCase):
 
-    def test_check_file_absent(self):        
-        if os.path.isfile(flowers.filename):
-            os.remove(flowers.filename)
+    def setUp(self):
+        self.cls = flowers.SurpriseFlowers
 
-        expected = {'flowerday':None, 'current_month':None, 'email_sent':False}
-        flowermap, file_status = flowers.check_file(flowers.filename)
-        self.assertEquals(flowermap, expected)
-        
-        with open(flowers.filename, 'r') as f:
-            flowermap = json.loads(f.read())
-        self.assertEquals(flowermap, expected)
-
-    # test the method that checks to see if it should send an email 
-    def test_check_emailer1(self):
-        todays_date = datetime.date(2013,6,24)
-        flowermap = {"flowerday": 23, "current_month": 6, "email_sent": True}
-        expected = 'email not sent'
-        self.assert_check_emailer(todays_date, flowermap, expected)
-
-    def test_check_emailer2(self):
-        todays_date = datetime.date(2013,6,13)
-        flowermap = {"flowerday": 23, "current_month": 6, "email_sent": False}
-        expected = 'email not sent'
-        self.assert_check_emailer(todays_date, flowermap, expected)
-    
-    def test_check_emailer3(self):
-        todays_date = datetime.date(2013,6,13)
-        flowermap = {"flowerday": 23, "current_month": 6, "email_sent": False}
-        expected = 'email not sent'
-        self.assert_check_emailer(todays_date, flowermap, expected)
-
-    def test_check_emailer4(self):
-        todays_date = datetime.date(2013,6,23)
-        flowermap = {"flowerday": 23, "current_month": 6, "email_sent": False}
-        expected = 'email sent'
-        self.assert_check_emailer(todays_date, flowermap, expected)
-
-    def test_check_emailer5(self):
-        todays_date = datetime.date(2013,5,23)
-        flowermap = {"flowerday": 23, "current_month": 6, "email_sent": True}
-        expected = 'email not sent'
-        self.assert_check_emailer(todays_date, flowermap, expected)
-
-    def test_check_emailer6(self):
-        todays_date = datetime.date(2013,5,23)
-        flowermap = {"flowerday": 23, "current_month": 6, "email_sent": False}
-        expected = 'email not sent'
-        self.assert_check_emailer(todays_date, flowermap, expected)
-
-    def assert_check_emailer(self, todays_date, flowermap, expected):
+    def test_check_date_for_emailer(self):
         def test_email_chris(flowermap, todays_date):
             return flowermap, todays_date
-        email_status = flowers.check_date_for_emailer(flowermap, todays_date, test_email_chris)
-        self.assertEquals(email_status, expected)
+            # test the method that checks to see if it should send an email
+        run_conditions = [[{"flowerday": 23, "current_month": 6, "email_sent": True}, 'email not sent', datetime.date(2013,6,24)],
+                      [{"flowerday": 23, "current_month": 6, "email_sent": False}, 'email not sent', datetime.date(2013,6,13)],
+                      [{"flowerday": 23, "current_month": 6, "email_sent": False}, 'email not sent', datetime.date(2013,6,13)],
+                      [{"flowerday": 23, "current_month": 6, "email_sent": False}, 'email sent', datetime.date(2013,6,23)],
+                      [{"flowerday": 23, "current_month": 6, "email_sent": True}, 'email not sent', datetime.date(2013,5,23)],
+                      [{"flowerday": 23, "current_month": 6, "email_sent": False}, 'email not sent', datetime.date(2013,5,23)]]
+        for flowermap, expected, todays_date in run_conditions:
+            email_status = self.cls(flowermap=flowermap, date=todays_date).check_date_for_emailer(test_email_chris)
+            self.assertEquals(email_status, expected)
 
-    # test the method that checks to see if it should generate a random date
-    def test_check_generator1(self):
-        todays_date = datetime.date(2013,6,23)
-        flowermap = {"flowerday": 23, "current_month": 6, "email_sent": False}
-        expected = 'date not generated'
-        self.assert_check_generator(flowermap, expected, todays_date)
+    def test_check_date_for_generator(self):
+        self.todays_date = datetime.date(2013,6,23)
+        run_conditions = [[{"flowerday": 23, "current_month": 6, "email_sent": False}, 'date not generated'],
+                      [{"flowerday": 24, "current_month": 6, "email_sent": True}, 'date not generated'],
+                      [{"flowerday": 23, "current_month": 5, "email_sent": False}, 'date was generated']]
 
-    def test_check_generator2(self):
-        todays_date = datetime.date(2013,6,23)
-        flowermap = {"flowerday": 24, "current_month": 6, "email_sent": True}
-        expected = 'date not generated'
-        self.assert_check_generator(flowermap, expected, todays_date)
+        for flowermap, expected in run_conditions:
+            flowermap, generator_status = self.cls(flowermap=flowermap, date=self.todays_date).check_date_for_generator()
+            self.assertEquals(generator_status, expected)
 
-    def test_check_generator3(self):
-        todays_date = datetime.date(2013,6,23)
-        flowermap = {"flowerday": 23, "current_month": 5, "email_sent": False}
-        expected = 'date was generated'
-        self.assert_check_generator(flowermap, expected, todays_date)
-
-    def assert_check_generator(self, flowermap, expected, todays_date):
-        flowermap, generator_status = flowers.check_date_for_generator(flowermap, todays_date)
-        self.assertEquals(generator_status, expected)
 
 if __name__ == '__main__':
     unittest.main()
